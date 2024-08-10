26496beea09e0d4e0cd6c082ca36f999642fe6c8
Prashish Paudel <prashishpaudel@Prashishs-MBP.lan>
2024-08-10 19:37:42 -0400

    first commit;

---
+++ module.js
@@ -0,0 +1,5 @@
+const {people:name,ages} = require('./people')
+console.log(name)
+
+const os = require('os')
+console.log(os.platform(), os.homedir())
---
+++ people.js
@@ -0,0 +1,3 @@
+const people = ['alex', 'banu', 'novita', 'ryan']
+const ages = [20,15,30,35]
+module.exports = {people,ages}
